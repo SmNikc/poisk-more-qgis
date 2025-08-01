b200e9af1ae98670d9da4c25ad56ed1595d27c5d
SmNikc <NNSmetanin@gmail.com>
2025-08-02 01:40:06 +0300

    Fix syntax errors and update database utilities

--- dialogs/dialog_auth.py
+++ dialogs/dialog_auth.py
@@ -1,3 +1,25 @@
-python from PyQt5.QtWidgets import QDialog, QMessageBox from PyQt5 import uic import os from .db_manager import DBManager
-class AuthDialog(QDialog): def init(self, parent=None): super().init(parent) uic.loadUi(os.path.join(os.path.dirname(file), '../forms/AuthForm.ui'), self) self.buttonLogin.clicked.connect(self.login)
-def login(self): user = self.editUser.text() password = self.editPassword.text() db = DBManager() if db.authenticate(user, password): QMessageBox.information(self, "Успех", "Авторизация прошла") self.accept() else: QMessageBox.warning(self, "Ошибка", "Неверный логин/пароль")
+"""Диалог авторизации пользователя."""
+
+from PyQt5.QtWidgets import QDialog, QMessageBox
+from PyQt5 import uic
+import os
+
+from ..utils.db_manager import DBManager
+
+
+class AuthDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        ui_path = os.path.join(os.path.dirname(__file__), "../forms/AuthForm.ui")
+        uic.loadUi(ui_path, self)
+        self.buttonLogin.clicked.connect(self.login)
+
+    def login(self):
+        user = self.editUser.text()
+        password = self.editPassword.text()
+        db = DBManager()
+        if db.authenticate(user, password):
+            QMessageBox.information(self, "Успех", "Авторизация прошла")
+            self.accept()
+        else:
+            QMessageBox.warning(self, "Ошибка", "Неверный логин/пароль")

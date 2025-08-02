a0326f3bffc74ead0d66e1d30536839fdf41f0ca
SmNick <nnsmetanin@gmail.com>
2025-08-03 00:26:08 +0300

    Удалена лишняя папка alg из корня, оставлены актуальные модули в poiskmore_plugin/alg

--- alg/alg_probabilities.py
+++
@@ -1,4 +0,0 @@
-python import numpy as np import math
-def compute_probability_map(lkp, radius, resolution, wind_speed, wind_dir, current_speed, current_dir, time_hours): drift_x, drift_y = calculate_drift(0, 0, wind_speed, wind_dir, current_speed, current_dir, time_hours) size = int((radius * 2) / resolution) center = size // 2 matrix = np.zeros((size, size))
-for i in range(size): for j in range(size): dx = (j - center) * resolution - drift_x dy = (i - center) * resolution - drift_y distance = math.sqrt(dx2 + dy2) matrix[i][j] = max(0, 1 - (distance / radius))
-return matrix

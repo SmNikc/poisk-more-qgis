65d5976c6dd23ac46ebf82fb1c846535ddaa612a
SmNikc <NNSmetanin@gmail.com>
2025-08-05 23:11:46 +0300

    Remove stray CopyEdit tokens

--- poiskmore_plugin/utils/case_archive.py
+++ poiskmore_plugin/utils/case_archive.py
@@ -1,4 +1,3 @@
-CopyEdit
 import json, datetime, os
 class CaseArchive:
     def __init__(self, path="archive_cases"):
--- poiskmore_plugin/utils/plan_search.py
+++ poiskmore_plugin/utils/plan_search.py
@@ -1,4 +1,3 @@
-CopyEdit
 class PlanSearchManager:
     def __init__(self):
         self.load_data()

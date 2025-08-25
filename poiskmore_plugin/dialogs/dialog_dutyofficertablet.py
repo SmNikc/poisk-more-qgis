9c4f21d35bffa795bab6b298611b98345a5c463b
SmNikc <NNSmetanin@gmail.com>
2025-08-25 17:08:59 +0300

    Fix missing duty tablet dialog

--- poiskmore_plugin/dialogs/dialog_dutyofficertablet.py
+++
@@ -1,8 +0,0 @@
-from PyQt5.QtWidgets import QDialog, QTableWidget, QVBoxLayout
-class DialogDutyOfficerTablet(QDialog):
-def __init__(self, parent=None):
-super().__init__(parent)
-layout = QVBoxLayout(self)
-table = QTableWidget(10, 5)
-table.setHorizontalHeaderLabels(["ID", "Описание", "Координаты", "Статус", "Дата"])
-layout.addWidget(table)
